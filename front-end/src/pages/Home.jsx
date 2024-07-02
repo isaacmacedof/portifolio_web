ec3e66c6c3c10b06aab119673d8f74ed6b6450ed
Isaac Macedo <isaacmacedotr@gmail.com>
2024-07-02 10:44:11 -0300

    style: :art: Header style

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import Header from "../components/Header";
+import '../styles/pages/Home.css';
+
 const Home = () => {
   return (
-    <div>
-      <h3>Olá! Me chamo Isaac,</h3>
-      <h1>Desenvolvedor Web</h1>
-      <p>Sou um desenvolvedor Full Stack brasileiro que adora estar por dentro das tecnologias! Atualmente desenvolvendo ainda mais meus conhecimentos em React, Docker, MySQL e Node.js </p>
-    </div>
+    <main className="home">
+      <div>
+        <Header />
+      </div>
+      <div className="main-containt">
+        <h3>Olá! Me chamo Isaac,</h3>
+        <h1>Desenvolvedor Web</h1>
+        <p>Sou um desenvolvedor Full Stack brasileiro que adora estar por dentro das tecnologias! Atualmente desenvolvendo ainda mais meus conhecimentos em React, Docker, MySQL e Node.js </p>
+      </div>
+    </main>
   )
 }
 
