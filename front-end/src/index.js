9d0130a1df73029864ef29827d3d355ad930c2e6
Isaac Macedo <isaacmacedotr@gmail.com>
2024-07-02 19:01:03 -0300

    feat: :sparkles: Home base finish

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './styles/system/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Helmet>
-      <title>My Portifólio</title>
-    </Helmet>
     <App />
   </React.StrictMode>
 );
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,17 +1,28 @@
 import Header from "../components/Header";
+import imgPro from "../images/img-program-true.jpg";
 import '../styles/pages/Home.css';
 
 const Home = () => {
   return (
     <main className="home">
-      <div>
-        <Header />
-      </div>
-      <div className="main-containt">
-        <h3>Olá! Me chamo Isaac,</h3>
-        <h1>Desenvolvedor Web</h1>
-        <p>Sou um desenvolvedor Full Stack brasileiro que adora estar por dentro das tecnologias! Atualmente desenvolvendo ainda mais meus conhecimentos em React, Docker, MySQL e Node.js </p>
-      </div>
+      <Header />
+      <section className="section-apresentation">
+        <article className="main-containt-apresentation">
+          <p id="apresentation-basic">Olá! Me chamo Isaac,</p>
+          <p id="apresentation-title">Web Developer</p>
+          <p id="apresentation-description">Sou um desenvolvedor Full Stack brasileiro que adora estar por dentro das tecnologias! Atualmente desenvolvendo ainda mais meus conhecimentos em React, Docker, MySQL e Node.js </p>
+          <button className="button-contact">
+          <div className="art-button">
+            <p className="text-button-contact">
+              Fale comigo agora!
+            </p>
+          </div>
+          </button>
+        </article>
+        <figure>
+          <img id="img-pro" src={ imgPro } alt="Programador" />
+        </figure>
+      </section>
     </main>
   )
 }
